abdde76b89552a40f3b69fbdc1d49effa115e17c
akram8 <akram868@163.com>
2022-02-15 23:56:11 +0800

    aaa

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  lang: 'en-US',
+  title: 'Hello, VitePress!',
+  description: 'This is my first VitePress site',
+  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  // theme and its config
+  // theme: '@vitepress/defaultTheme',
+  themeConfig: {
+    logo: '/favicon.ico',
+    // logo: 'https://vuejs.org/images/logo.png',
+    darkMode: true
+  }
+}
